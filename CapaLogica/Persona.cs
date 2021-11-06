de0999369835af9fa9fde0e8d340a24d4859e0e5
Kevin Arias <kevinjosuearias@hotmail.com>
2021-11-06 16:34:22 -0600

    Creacion de clase padre Persona y eliminacion de archivo vacio prueba

---
+++ CapaLogica/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class Persona
+    {
+        public string cedula { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public int telefono { get; set; }
+    }
+}
