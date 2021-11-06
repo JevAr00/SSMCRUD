d3905ad75922a786ad66dd339166dd894f67d349
Kevin Arias <kevinjosuearias@hotmail.com>
2021-11-06 16:36:13 -0600

    Creacion de interfaz CRUD

---
+++ CapaLogica/ICrud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    interface ICrud<T>
+    {
+        void Create(T obj);
+        void Read(T obj);
+        void Update(T obj);
+        void Delete(T obj);
+    }
+}
