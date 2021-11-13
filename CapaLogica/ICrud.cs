32a3948620d9987706d68beee1899e388d785f0c
Kevin Arias <kevinjosuearias@hotmail.com>
2021-11-12 18:22:20 -0600

    documentacion codigo ICrud

--- CapaLogica/ICrud.cs
+++ CapaLogica/ICrud.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace CapaLogica
 {
+
+    /// <summary>
+    /// Define los metodos principales de CRUD
+    /// </summary>
+    /// <typeparam name="T">Entidad de base de datos</typeparam>
     public interface ICrud<T>
     {
+        /// <param name="obj">Objeto de tipo <typeparamref name="T"/></param>
         void Create(T obj);
+
+        /// <param name="obj">Objeto de tipo <typeparamref name="T"/></param>
         void Read(T obj);
+
+        /// <param name="obj">Objeto de tipo <typeparamref name="T"/></param>
         void Update(T obj);
+
+        /// <param name="obj">Objeto de tipo <typeparamref name="T"/></param>
         void Delete(T obj);
     }
 }
