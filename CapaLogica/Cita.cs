68ec68eab6fabb3a484cf3e1c0d2a2c090199aae
Kevin Arias <kevinjosuearias@hotmail.com>
2021-11-24 16:22:03 -0600

    creacion objetos citas y areas

---
+++ CapaLogica/Cita.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class Cita:Persona
+    {
+        public int idCita { get; set; }
+        public string descripcion { get; set; }
+        public string nombreDoctor { get; set; }
+        public string fecha { get; set; }
+        public string hora { get; set; }
+    }
+}
