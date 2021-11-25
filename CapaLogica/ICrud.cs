64ef64721ef85d43d68da025af0353a561e8b379
Kevin Arias <kevinjosuearias@hotmail.com>
2021-11-25 15:14:42 -0600

    Comentarios ICrud

--- CapaLogica/ICrud.cs
+++ CapaLogica/ICrud.cs
@@ -6,26 +6,34 @@ using System.Threading.Tasks;
 
 namespace CapaLogica
 {
-
-    /// <summary>
-    /// Define los metodos principales de CRUD
-    /// </summary>
-    /// <typeparam name="T">Entidad de base de datos</typeparam>
+    ///<summary>
+    ///Define los metodos principales de CRUD
+    ///</summary>
+    ///<typeparam name="T">Entidad de base de datos</typeparam>
     public interface ICrud<T>
     {
         ///<summary>
-        ///Inserta datos en la tabla <see cref="ICrud"/>
+        ///Inserta informacion en la base de datos
         ///</summary>
-        /// <param name="obj">Objeto de tipo <typeparamref name="T"/></param>
+        ///<param name="obj">Objeto de tipo <typeparamref name="T"/></param>
         bool Create(T obj);
 
-        /// <param name="obj">Objeto de tipo <typeparamref name="T"/></param>
+        ///<summary>
+        ///Lee informacion de la base de datos
+        ///</summary>
+        ///<param name="obj">Objeto de tipo <typeparamref name="T"/></param>
         void Read(T obj);
 
-        /// <param name="obj">Objeto de tipo <typeparamref name="T"/></param>
+        ///<summary>
+        ///Modifica informacion en la base de datos
+        ///</summary>
+        ///<param name="obj">Objeto de tipo <typeparamref name="T"/></param>
         bool Update(T obj);
 
-        /// <param name="obj">Objeto de tipo <typeparamref name="T"/></param>
+        ///<summary>
+        ///Elimina informacion en la base de datos
+        ///</summary>
+        ///<param name="obj">Objeto de tipo <typeparamref name="T"/></param>
         bool Delete(T obj);
     }
 }
