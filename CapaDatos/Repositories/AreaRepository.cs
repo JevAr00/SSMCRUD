2c51dc6bb1db2eba8d142efda6240b34e3f2db4f
Kevin Arias <kevinjosuearias@hotmail.com>
2021-11-29 14:48:38 -0600

    hotfix sentencia sql

--- CapaDatos/Repositories/AreaRepository.cs
+++ CapaDatos/Repositories/AreaRepository.cs
@@ -20,7 +20,7 @@ namespace CapaDatos.Repositories
         public AreaRepository()
         {
             selectAll = "select * from areas";
-            insert = "insert into areas values(@nombre,@edificio,@habilitada)";
+            insert = "insert into areas values(default,@nombre,@edificio,@habilitada)";
             update = "update areas set nombre=@nombre, edificio=@edificio, habilitada=@habilitada where idArea=@id";
             delete = "delete from areas where idArea=@id";
         }
