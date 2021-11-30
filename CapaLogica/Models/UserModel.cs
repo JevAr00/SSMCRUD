1a0300ceb2f8e6a7d15ecf52bf4d6f3170b9ac7a
Kevin Arias <kevinjosuearias@hotmail.com>
2021-11-29 18:21:42 -0600

    Creacion userModel y primera prueba metodo de crifrado

---
+++ CapaLogica/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CapaLogica.Models
+{
+    public class UserModel
+    {
+        public string Encrypt(string pwrd)
+        {
+            SHA512 hashSvc = SHA512.Create();
+            byte[] hash = hashSvc.ComputeHash(Encoding.UTF8.GetBytes(pwrd));
+            return Convert.ToBase64String(hash).ToUpper();
+        }
+    }
+}
