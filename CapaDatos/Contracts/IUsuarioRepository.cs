8c0a5b9509449b1401c5da160a8ebc36746cc6fb
Kevin Arias <kevinjosuearias@hotmail.com>
2021-11-30 14:08:12 -0600

    Se declara metodo para login

--- CapaDatos/Contracts/IUsuarioRepository.cs
+++ CapaDatos/Contracts/IUsuarioRepository.cs
@@ -13,5 +13,10 @@ namespace CapaDatos.Contracts
     /// <remarks>Implementa <see cref="IGenericRepository{Entity}"/></remarks>
     public interface IUsuarioRepository:IGenericRepository<Usuario>
     {
+        /// <summary>
+        /// Verifica si un usuario existe en la base de datos
+        /// </summary>
+        /// <returns>Si usuario buscado esta registrado</returns>
+        bool Login(Usuario Entity);
     }
 }
