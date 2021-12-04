46c34d5973e16d2cf3b73d3ad1a7828cc90ddcd2
Kevin Arias <kevinjosuearias@hotmail.com>
2021-12-04 14:45:41 -0600

    Personas ahora es abstracto. Solo puede ser heredado

--- CapaDatos/Entities/Persona.cs
+++ CapaDatos/Entities/Persona.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CapaDatos.Entities
 {
-    public class Persona
+    public abstract class Persona
     {
         public string cedula { get; set; }
         public string nombre { get; set; }
