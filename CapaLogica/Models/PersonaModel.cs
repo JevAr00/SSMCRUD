b1070a1ecdfd7ba3f4427af3fb5abd938e07eac6
Kevin Arias <kevinjosuearias@hotmail.com>
2021-12-06 12:00:40 -0600

    PersonaModel

--- CapaLogica/Models/PersonaModel.cs
+++ CapaLogica/Models/PersonaModel.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace CapaLogica.Models
 {
-    class PersonaModel
+    public abstract class PersonaModel
     {
+        private string cedula;
+        private string nombre;
+        private string apellidos;
+        private int telefono;
+
+        public string Cedula { get => cedula; set => cedula = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public int Telefono { get => telefono; set => telefono = value; }
     }
 }
