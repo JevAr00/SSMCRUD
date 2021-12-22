ce34d69dcb541da0d479d863a5e422ebdc1e72c9
Kevin Arias <kevinjosuearias@hotmail.com>
2021-12-22 11:01:09 -0600

    Liempieza de usings no utilizados

--- CapaLogica/Models/CitaModel.cs
+++ CapaLogica/Models/CitaModel.cs
@@ -4,8 +4,6 @@ using CapaDatos.Repositories;
 using CapaLogica.ValueObjects;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CapaLogica.Models
